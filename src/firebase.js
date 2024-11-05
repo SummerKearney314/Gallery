c7134aef6b57efce8782855308cd2a6453bee1dd
SummerKearney314 <54952555+SummerKearney314@users.noreply.github.com>
2024-11-05 11:45:28 -0500

    Error with ProjectID

--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,8 @@ import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
+console.log("Firebase Config:", process.env.REACT_APP_FIREBASE_API_KEY);
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -10,6 +12,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
